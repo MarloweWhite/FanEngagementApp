a0f1a5de95f38a47becfe74a90fefc3bc3f04570
Marlowe White <marlowe_white@yahoo.co.uk>
2018-08-22 20:13:09 +0100

    small fix

--- app/src/main/java/com/example/marlowe/roarsports/FragmentShare.java
+++ app/src/main/java/com/example/marlowe/roarsports/FragmentShare.java
@@ -5,33 +5,22 @@ import android.content.ClipboardManager;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
 import com.facebook.share.model.ShareLinkContent;
-import com.facebook.share.model.SharePhoto;
-import com.facebook.share.model.SharePhotoContent;
 import com.facebook.share.widget.ShareDialog;
-import com.squareup.picasso.Picasso;
 import com.twitter.sdk.android.tweetcomposer.TweetComposer;
 
-import java.io.File;
-import java.io.InputStream;
-import java.lang.annotation.Target;
-import java.net.URL;
 
 public class FragmentShare  extends Fragment {
 
@@ -93,7 +82,6 @@ public class FragmentShare  extends Fragment {
             @Override
             public void onClick(View v) {
                 String type = "image/*";
-
 
                 Intent share = new Intent(Intent.ACTION_SEND);
 
